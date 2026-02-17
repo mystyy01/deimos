3ec1fcee855b8a4ac62ecce524c868ff8eaefdd7
mystyy01 <x0amified@gmail.com>
2026-02-17 16:29:27 +0000

    sync deimos changes

--- main.c
+++ main.c
@@ -5,21 +5,25 @@ int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
-    write(STDOUT, "[deimos] start-write\n", 21);
-    print("[deimos] start-print\n");
+    print("[deimos] starting\n");
 
-    if (render_init() != 0) {
+    int rc = render_init();
+    if (rc != 0) {
+        print("[deimos] render_init FAILED\n");
         exit(1);
         return 1;
     }
+    print("[deimos] render_init ok\n");
 
     // First visible bring-up frame.
     render_begin_frame(0x101820);
+    print("[deimos] begin_frame ok\n");
     render_fill_rect(40, 40, 120, 70, 0x00AA66);
+    print("[deimos] fill_rect ok\n");
     render_draw_rect(40, 40, 120, 70, 0xFFFFFF);
+    print("[deimos] draw_rect ok\n");
     render_end_frame();
-
-    print("deimos: frame drawn (press q to quit)\n");
+    print("[deimos] end_frame ok\n");
 
     while (1) {
         struct user_input_event ev;
--- rendering/rendering.c
+++ rendering/rendering.c
@@ -1,89 +1,77 @@
 #include "rendering.h"
-#include "../../apps/libsys.h" // should prolly copy over the header file into this so it doesnt rely on the exact file structure of this repo
+#include "../../apps/libsys.h"
 
 static struct user_fb_info g_fb;
+static uint32_t *backbuffer;
 
 int render_init(void) {
     int rc = fb_info(&g_fb);
-    if (rc != 0) {
-        return -1;
-    }
+    if (rc != 0) return -1;
+    if (g_fb.width == 0 || g_fb.height == 0) return -1;
 
-    if (g_fb.width == 0 || g_fb.height == 0) {
-        return -1;
-    }
+    long addr = fb_map();
+    if (!addr) return -1;
+    backbuffer = (uint32_t *)addr;
 
     return 0;
 }
-int render_width(void){
-    return (int)g_fb.width;
-}
-int render_height(void){
-    return (int)g_fb.height;
-}
-int render_bpp(void){
-    return (int)g_fb.bpp;    
-}
-int render_pitch(void){
-    return (int)g_fb.pitch;
-}
+
+int render_width(void)  { return (int)g_fb.width; }
+int render_height(void) { return (int)g_fb.height; }
+int render_bpp(void)    { return (int)g_fb.bpp; }
+int render_pitch(void)  { return (int)g_fb.pitch; }
 
 void render_begin_frame(uint32_t clear_colour) {
-    (void)clear_colour;
-    // Full-screen clear via fb_putpixel syscall is too slow for now.
-    // TODO(HUMAN): Re-enable once SYS_FB_PRESENT/backbuffer path exists.
-}
-
-void render_end_frame(void) {
-    // TODO(HUMAN): Present/backbuffer logic will go here.
-}
-
-void render_putpixel(int x, int y, uint32_t colour) {
-    if (x < 0 || y < 0) return;
-    if (x >= render_width() || y >= render_height()) return;
-    fb_putpixel(x, y, colour);
-}
-
-void render_clear(uint32_t colour) {
-    for (int y = 0; y < render_height(); y++) {
-        for (int x = 0; x < render_width(); x++) {
-            render_putpixel(x, y, colour);
-        }
+    int total = g_fb.width * g_fb.height;
+    for (int i = 0; i < total; i++) {
+        backbuffer[i] = clear_colour;
     }
 }
 
+void render_end_frame(void) {
+    fb_present(backbuffer);
+}
+
+void render_putpixel(int x, int y, uint32_t colour) {
+    if ((unsigned)x >= g_fb.width || (unsigned)y >= g_fb.height) return;
+    backbuffer[y * g_fb.width + x] = colour;
+}
+
+void render_clear(uint32_t colour) {
+    render_begin_frame(colour);
+}
+
 void render_fill_rect(int x, int y, int w, int h, uint32_t colour) {
-    for (int yy = 0; yy < h; yy++) {
-        for (int xx = 0; xx < w; xx++) {
-            render_putpixel(x + xx, y + yy, colour);
+    int x0 = x < 0 ? 0 : x;
+    int y0 = y < 0 ? 0 : y;
+    int x1 = x + w > (int)g_fb.width  ? (int)g_fb.width  : x + w;
+    int y1 = y + h > (int)g_fb.height ? (int)g_fb.height : y + h;
+
+    for (int yy = y0; yy < y1; yy++) {
+        uint32_t *row = backbuffer + yy * g_fb.width + x0;
+        int count = x1 - x0;
+        for (int i = 0; i < count; i++) {
+            row[i] = colour;
         }
     }
 }
 
 void render_draw_rect(int x, int y, int w, int h, uint32_t colour) {
+    // Top and bottom edges
     for (int xx = 0; xx < w; xx++) {
         render_putpixel(x + xx, y, colour);
         render_putpixel(x + xx, y + h - 1, colour);
     }
+    // Left and right edges
     for (int yy = 0; yy < h; yy++) {
         render_putpixel(x, y + yy, colour);
         render_putpixel(x + w - 1, y + yy, colour);
     }
 }
 
-// needs font agnostic functions
-
-void render_mark_dirty(int x, int w, int h){
-    (void)x;
-    (void)w;
-    (void)h;
+void render_mark_dirty(int x, int w, int h) {
+    (void)x; (void)w; (void)h;
 }
-void render_reset_dirty(void){
-
-}
-void render_present_full(void){
-
-}
-void render_present_dirty(void){
-
-}
+void render_reset_dirty(void) {}
+void render_present_full(void) { render_end_frame(); }
+void render_present_dirty(void) { render_end_frame(); }
